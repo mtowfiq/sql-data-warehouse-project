cb0dd5aaebca4e769ef1b6e85650b94b93c8407e
Mohammad Towfiqul Islam <93540557+mtowfiq@users.noreply.github.com>
2025-11-03 05:49:18 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,11 @@
+/*
+The purpose of this file is to create a database named 'datawarehouse' and also create schemas. 
+*/
+
+CREATE DATABASE datawarehouse;
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
